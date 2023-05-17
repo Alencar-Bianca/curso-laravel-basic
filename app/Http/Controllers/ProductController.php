91ad798997686d1000be50cff97dd20fcf9479d4
Alencar-Bianca <alencar-bianca@hotmail.com>
2023-05-16 22:18:17 -0300

    criacao de blade, section, extends e yield

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $nome = 'bianca';
+        return view('web.home',compact('nome'));
     }
 
     /**
---
+++ resources/views/web/home.blade.php
@@ -0,0 +1,5 @@
+@extends('web.layouts.default')
+@section('title', 'Home produto')
+@section('content')
+  <h1>oioio</h1>
+@endsection
